20a299f0a891d7db25e5fdb538be14d7e7dced3e
CarrotKnits <carrotsandcarrots@gmail.com>
2024-11-15 17:28:25 -0500

    create game loop for blk screen and quit button[x] functionality

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
 
 def main():
-	print("Starting asteroids!")
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(0, rect=None, special_flags=0)
+        pygame.display.flip()
 
 
 
