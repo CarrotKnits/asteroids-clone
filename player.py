a833919ac6464009e6ee0ae36e3767fd213cb748
CarrotKnits <carrotsandcarrots@gmail.com>
2024-11-26 14:51:37 -0500

    added 'move up' & 'move down' functionality

--- player.py
+++ player.py
@@ -32,3 +32,13 @@ class Player(CircleShape):
         # Turn right
         if keys[pygame.K_d]:
             self.rotate(dt)
+        # Move up
+        if keys[pygame.K_w]:
+            self.move(dt)
+        # Move down
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
